395942fe1c5c93007df21c03b1d78a41b8022907
Ankur Gupta <ankurnecessary@gmail.com>
2023-07-28 05:59:54 +0930

    Lecture 86: Rendering lists of data

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -13,38 +13,19 @@ const Expenses = (props) => {
         console.log("Expenses.js: ", year);
     }
 
-    let filterInfoText = "2019, 2021 and 2022";
-    if (selectedYear === '2019') {
-        filterInfoText = "2020, 2021 and 2022";
-    }
-    else if (selectedYear === '2021') {
-        filterInfoText = "2019, 2020 and 2022";
-    }
-    else {
-        filterInfoText = "2019, 2020 and 2021";
-    }
-
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter yearSelected={selectedYear} onYearChange={yearChangeHandler} />
-                {filterInfoText}
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date} />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date} />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date} />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date} />
+                {
+                    props.items.map((expense) => (
+                        <ExpenseItem
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    ))
+                }
             </Card>
         </div>
     );
