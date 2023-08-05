4342c913bdc51c55b673733e7ca633f42dd8275b
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-05 12:56:15 +0930

    Lecture 147: Using the useEffect Cleanup Function

--- 07-starting-project_lecture144/src/components/Login/Login.js
+++ 07-starting-project_lecture144/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const validationTimeout = setTimeout(() => {
+      console.log('Validating username and password');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('Reset validation timer');
+      clearTimeout(validationTimeout);
+    }
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
