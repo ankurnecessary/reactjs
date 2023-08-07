0b2f8c7d1880629b9b54a057597338a616f9ed42
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-07 13:38:03 +0930

    Lecture 158: Building & Using a Custom Context Provider Component

--- 07-starting-project_lecture144/src/components/Login/Login.js
+++ 07-starting-project_lecture144/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (previousState, action) => {
   if (action.type === 'USER_INPUT') {
@@ -26,11 +27,12 @@ const passwordReducer = (previousState, action) => {
   return { value: '', isValid: false };
 }
 
-const Login = (props) => {
+const Login = () => {
 
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
+  const ctx = useContext(AuthContext);
 
   useEffect(() => {
     const validationTimeout = setTimeout(() => {
@@ -64,7 +66,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- 07-starting-project_lecture144/src/store/auth-context.js
+++ 07-starting-project_lecture144/src/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // React.createContext will return an object that has a component too.
 const AuthContext = React.createContext({
     isLoggedIn: false
+    , onLogout: () => { }
+    , onLogin: (username, password) => { }
 });
+
+export const AuthContextProvider = (props) => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const loginHandler = (username, password) => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn
+            , onLogin: loginHandler
+            , onLogout: logoutHandler
+        }} >
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
 
 export default AuthContext;
