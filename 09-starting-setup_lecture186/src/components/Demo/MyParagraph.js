01a8691f017e29b8c6f8525a1833ff666f01d095
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-12 12:38:00 +0930

    Lecture 187: A Closer Look At Child Component Re-Evaluation

---
+++ 09-starting-setup_lecture186/src/components/Demo/MyParagraph.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DemoOutput = (props) => {
+
+    console.log('MyParagraph RUNNING');
+
+    return (
+        <p>{props.children}</p>
+    );
+}
+
+export default DemoOutput;
