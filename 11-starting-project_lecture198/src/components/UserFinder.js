a1cc4dab2b4beffe1d29bfa2a18d4cc0555e0cf0
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-14 11:40:49 +0930

    Lecture 201: Lifecycle Methods In Action

--- 11-starting-project_lecture198/src/components/UserFinder.js
+++ 11-starting-project_lecture198/src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -9,7 +9,54 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const UserFinder = () => {
+class UserFinder extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      filteredUsers: DUMMY_USERS
+    })
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+
+    if (prevState.searchTerm === this.state.searchTerm) return;
+
+    this.setState({
+      filteredUsers: DUMMY_USERS.filter((user) => {
+        return user.name.includes(this.state.searchTerm);
+      })
+    });
+
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+
+}
+
+/*const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -31,6 +78,6 @@ const UserFinder = () => {
       <Users users={filteredUsers} />
     </Fragment>
   );
-};
+};*/
 
 export default UserFinder;
