f1b90fbe6616675ffc964c9b25ee338d6dca661e
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-15 06:53:56 +0930

    Lecture 214: Using useEffect() For Requests

--- 12-starting-project_lecture210/src/App.js
+++ 12-starting-project_lecture210/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,33 +8,40 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(() => {
+    async () => {
 
-    setError(null);
-    setIsLoading(true);
+      setError(null);
+      setIsLoading(true);
 
-    try {
-      const response = await fetch('https://swapi.dev/api/films');
-      if (!response.ok) { // Error check should be done after fetch call
-        throw new Error("Something went wrong!");
+      try {
+        const response = await fetch('https://swapi.dev/api/films');
+        if (!response.ok) { // Error check should be done after fetch call
+          throw new Error("Something went wrong!");
+        }
+        const data = await response.json();
+        const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
+          return {
+            id: episode_id,
+            title,
+            releaseDate: release_date,
+            openingText: opening_crawl
+          };
+        })
+        setMovies(transformedMovies);
       }
-      const data = await response.json();
-      const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
-        return {
-          id: episode_id,
-          title,
-          releaseDate: release_date,
-          openingText: opening_crawl
-        };
-      })
-      setMovies(transformedMovies);
-    }
-    catch (error) {
-      setError(error.message);
-    }
+      catch (error) {
+        setError(error.message);
+      }
 
-    setIsLoading(false);
-  }
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  // If there are any external dependencies then fetchMoviesHandler could change it's state
 
   let content = <p>Found no movies!</p>;
 
