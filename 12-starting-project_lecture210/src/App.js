96fd270997d30d7c7dc1d61c8f0026ef8bae9650
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-15 05:25:39 +0930

    Lecture 211: Using async / await

--- 12-starting-project_lecture210/src/App.js
+++ 12-starting-project_lecture210/src/App.js
@@ -6,22 +6,18 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
-          return {
-            id: episode_id,
-            title,
-            releaseDate: release_date,
-            openingText: opening_crawl
-          };
-        })
-        setMovies(transformedMovies);
-      })
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
+      return {
+        id: episode_id,
+        title,
+        releaseDate: release_date,
+        openingText: opening_crawl
+      };
+    })
+    setMovies(transformedMovies);
   }
 
   return (
