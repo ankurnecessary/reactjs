06d7a8220a35785db6e9b99eca2f181171f08308
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-16 06:00:28 +0930

    Lecture 214: Using useEffect() For Requests. Error detected and resolved.

--- 12-starting-project_lecture210/src/App.js
+++ 12-starting-project_lecture210/src/App.js
@@ -8,34 +8,32 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(() => {
-    async () => {
+  const fetchMoviesHandler = useCallback(async () => {
 
-      setError(null);
-      setIsLoading(true);
+    setError(null);
+    setIsLoading(true);
 
-      try {
-        const response = await fetch('https://swapi.dev/api/films');
-        if (!response.ok) { // Error check should be done after fetch call
-          throw new Error("Something went wrong!");
-        }
-        const data = await response.json();
-        const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
-          return {
-            id: episode_id,
-            title,
-            releaseDate: release_date,
-            openingText: opening_crawl
-          };
-        })
-        setMovies(transformedMovies);
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) { // Error check should be done after fetch call
+        throw new Error("Something went wrong!");
       }
-      catch (error) {
-        setError(error.message);
-      }
-
-      setIsLoading(false);
+      const data = await response.json();
+      const transformedMovies = data.results.map(({ episode_id, title, release_date, opening_crawl }) => {
+        return {
+          id: episode_id,
+          title,
+          releaseDate: release_date,
+          openingText: opening_crawl
+        };
+      })
+      setMovies(transformedMovies);
     }
+    catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
