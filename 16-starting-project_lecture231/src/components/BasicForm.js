79028789665399f940c6475bfb358764def8a9d6
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-21 15:32:13 +0930

    Just added some spacing to make sense of HTML

--- 16-starting-project_lecture231/src/components/BasicForm.js
+++ 16-starting-project_lecture231/src/components/BasicForm.js
@@ -2,23 +2,32 @@ const BasicForm = (props) => {
   return (
     <form>
       <div className='control-group'>
+
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
           <input type='text' id='name' />
+          <p className="error-text">Please enter first name</p>
         </div>
+
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' />
+          <p className="error-text">Please enter last name</p>
         </div>
+
       </div>
+
       <div className='form-control'>
         <label htmlFor='name'>E-Mail Address</label>
         <input type='text' id='name' />
+        <p className="error-text">Please enter a valid email address</p>
       </div>
+
       <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
+
   );
 };
 
