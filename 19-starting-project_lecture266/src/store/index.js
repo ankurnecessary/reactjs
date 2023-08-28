a6c0ce825af73e623b6a41a0283d87ebcc2b5721
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-28 12:44:08 +0930

    Lecture 277: Connecting Redux Toolkit State

--- 19-starting-project_lecture266/src/store/index.js
+++ 19-starting-project_lecture266/src/store/index.js
@@ -1,9 +1,8 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: 'counter'
   , initialState
   , reducers: {
@@ -12,46 +11,10 @@ createSlice({
     , increase(state, action) { state.counter = state.counter + action.amount }
     , toggleCounter(state) { state.showCounter = !state.showCounter }
   }
-})
+});
 
-const counterReducer = (state = initialState, action) => {
-
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1
-      , showCounter: state.showCounter
-    }
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount
-      , showCounter: state.showCounter
-    }
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1
-      , showCounter: state.showCounter
-    }
-  }
-
-  if (action.type === 'toggle') {
-    /*
-    XXX This is a very bad practice XXX
-    state.showCounter = !state.showCounter;
-    return state;
-    */
-    return {
-      ...state
-      , showCounter: !state.showCounter
-    }
-  }
-
-  return state;
-}
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;
