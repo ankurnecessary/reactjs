f55d481c938e8c2fa23c2e864be8af33d79e29f4
Ankur Gupta <ankurnecessary@gmail.com>
2023-08-30 17:10:03 +0930

    Lecture 294: Using an Action Creator Thunk

--- 21-starting-project_lecture286_with_author/src/App.js
+++ 21-starting-project_lecture286_with_author/src/App.js
@@ -4,14 +4,13 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { uiSliceActions } from './store/ui-slice';
 import { Fragment } from 'react';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
 function App() {
-
   const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
@@ -19,43 +18,12 @@ function App() {
 
   useEffect(() => {
 
-    const sendCartData = async () => {
-
-      dispatch(uiSliceActions.showNotification({
-        status: 'pending'
-        , title: 'Sending...'
-        , message: 'Sending cart data'
-      }));
-
-      const response = await fetch('https://react-http-1b987-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT'
-        , body: JSON.stringify(cart)
-      })
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      dispatch(uiSliceActions.showNotification({
-        status: 'success'
-        , title: 'Success!'
-        , message: 'Sent cart data successfully!'
-      }))
-
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(uiSliceActions.showNotification({
-        status: 'error'
-        , title: 'Error!'
-        , message: 'Sending cart data failed!'
-      }));
-    });
+    dispatch(sendCartData(cart));
 
   }, [cart, dispatch]);
 
--- 21-starting-project_lecture286_with_author/src/store/cart-slice.js
+++ 21-starting-project_lecture286_with_author/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiSliceActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: 'cart'
@@ -40,6 +41,52 @@ const cartSlice = createSlice({
     }
   }
 })
+
+export const sendCartData = (cart) => {
+
+  return async (dispatch) => {
+
+    dispatch(uiSliceActions.showNotification({
+      status: 'pending'
+      , title: 'Sending...'
+      , message: 'Sending cart data'
+    }));
+
+    const sendRequest = async () => {
+
+      const response = await fetch('https://react-http-1b987-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT'
+        , body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+    }
+
+    try {
+      await sendRequest();
+
+      dispatch(uiSliceActions.showNotification({
+        status: 'success'
+        , title: 'Success!'
+        , message: 'Sent cart data successfully!'
+      }));
+    }
+    catch (error) {
+
+      dispatch(uiSliceActions.showNotification({
+        status: 'error'
+        , title: 'Error!'
+        , message: 'Sending cart data failed!'
+      }));
+
+    }
+
+  };
+
+}
 
 export const cartSliceActions = cartSlice.actions;
 
