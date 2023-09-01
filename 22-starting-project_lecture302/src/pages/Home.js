10e880706be43a35976b31687c6647f30639ee40
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-01 12:45:58 +0930

    Lecture 306: Navigating between Pages with Links

--- 22-starting-project_lecture302/src/pages/Home.js
+++ 22-starting-project_lecture302/src/pages/Home.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <h1>My Home Page</h1>
+  return (<>
+    <h1>My Home Page</h1>
+    <p>Go to the <Link to="/products">list of products</Link></p>
+  </>);
 }
 
 export default HomePage;
