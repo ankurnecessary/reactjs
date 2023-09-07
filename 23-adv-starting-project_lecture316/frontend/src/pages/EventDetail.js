b922964bbe85088b449fe3118ad3a8b5cfe01855
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-07 16:54:24 +0930

    Lecture 333: Submitting Data Programmatically

--- 23-adv-starting-project_lecture316/frontend/src/pages/EventDetail.js
+++ 23-adv-starting-project_lecture316/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
@@ -24,3 +24,19 @@ export const loader = async ({ request, params }) => {
     return response;
   }
 };
+
+export const action = async ({ request, params }) => {
+  const id = params.eventId;
+  const response = await fetch(`http://localhost:8080/events/${id}`, {
+    method: request.method
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not delete event.' }
+      , { status: 500 }
+    )
+  }
+
+  return redirect('/events');
+}
