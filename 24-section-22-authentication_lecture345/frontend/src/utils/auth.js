774aa2bbbd9cc620c2330fb2ba81af61ad306cf0
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-12 11:16:32 +0930

    Lecture 353: Updating the UI Based on Auth Status

--- 24-section-22-authentication_lecture345/frontend/src/utils/auth.js
+++ 24-section-22-authentication_lecture345/frontend/src/utils/auth.js
@@ -2,3 +2,7 @@ export const getAuthorizationToken = () => {
   const token = localStorage.getItem('token');
   return token;
 }
+
+export const tokenLoader = () => {
+  return getAuthorizationToken();
+}
