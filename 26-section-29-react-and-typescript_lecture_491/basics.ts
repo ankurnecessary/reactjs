858a656f1ad2ead1a42245e2249dcd47e3a7d5fb
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-19 11:52:19 +0930

    Lecture 492: Exploring the Base Types

---
+++ 26-section-29-react-and-typescript_lecture_491/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, string, boolean
+// More complex: objects, arrays
+// Function type, parameters
+
+let age: number = 22;
+
+age = 20;
+
+let username: string;
+
+username = 'Ankur';
+
+let isInstructor: boolean;
+
+isInstructor = true;
