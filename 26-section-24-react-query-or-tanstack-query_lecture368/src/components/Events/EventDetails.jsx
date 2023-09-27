9d69cfd90ceb25be2bb73226dbfa49ef5581c9f0
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-27 09:42:14 +0930

    Lecture 380: Disabling Automatic Refetching After Invalidations

--- 26-section-24-react-query-or-tanstack-query_lecture368/src/components/Events/EventDetails.jsx
+++ 26-section-24-react-query-or-tanstack-query_lecture368/src/components/Events/EventDetails.jsx
@@ -19,7 +19,10 @@ export default function EventDetails() {
   const { mutate } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["events"] });
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
       navigate("/events");
     },
   });
