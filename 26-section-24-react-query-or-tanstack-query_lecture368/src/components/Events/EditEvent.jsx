2fa984dbbf2c58e762dbfd8c30f3759d5a43a656
Ankur Gupta <ankurnecessary@gmail.com>
2023-09-27 12:15:05 +0930

    Lecture 383: Updating Data with Mutations

--- 26-section-24-react-query-or-tanstack-query_lecture368/src/components/Events/EditEvent.jsx
+++ 26-section-24-react-query-or-tanstack-query_lecture368/src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent } from "../../utils/http.js";
+import { fetchEvent, updateEvent } from "../../utils/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
@@ -13,8 +13,14 @@ export default function EditEvent() {
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
